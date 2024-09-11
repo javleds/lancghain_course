9cbfff5c53dd30ff8ee15ad40e8a91203d07293d
Javier Ledezma <juls0593@gmail.com>
2024-09-10 20:29:09 -0600

    Fix unit 2, 4th script

--- unit_3_data_connectiors/04_chunks_and_vectors.py
+++ unit_3_data_connectiors/04_chunks_and_vectors.py
@@ -1,126 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.document_loaders import CSVLoader
 
-# ## Importar librerías e instancia de modelo de chat
-
-# In[ ]:
-
-
-from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate,ChatPromptTemplate, HumanMessagePromptTemplate
-from langchain_openai import ChatOpenAI
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-chat = ChatOpenAI(openai_api_key=api_key)
-
-
-# #  Incrustación de texto (embedding)
-
-# In[ ]:
-
-
-from langchain_openai import OpenAIEmbeddings
-
-
-# In[ ]:
-
-
-embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-
-
-# In[ ]:
-
+load_dotenv()
+chat = ChatOpenAI()
+embeddings = OpenAIEmbeddings()
 
 texto = "Esto es un texto enviado a OpenAI para ser incrustado en un vector n-dimensional"
-
-
-# In[ ]:
-
-
 embedded_text = embeddings.embed_query(texto)
 
+print('\n Type of embedded_text:')
+print(type(embedded_text))
 
-# In[ ]:
+print('\n Embedded text:')
+print(embedded_text)
 
-
-type(embedded_text)
-
-
-# In[ ]:
-
-
-embedded_text
-
-
-# ## Incrustación de documentos
-
-# In[ ]:
-
-
-from langchain.document_loaders import CSVLoader
-
-
-# In[ ]:
-
-
-loader = CSVLoader('Fuentes datos/datos_ventas_small.csv',csv_args={'delimiter': ';'})
-
-
-# In[ ]:
-
-
+loader = CSVLoader('data_sources/datos_ventas_small.csv', csv_args={'delimiter': ';'})
 data = loader.load()
+print('\n Type of CSV data:')
+print(type(data))
+print('\n data at 0:')
+print(type(data[0]))
 
-
-# In[ ]:
-
-
-type(data)
-
-
-# In[ ]:
-
-
-type(data[0])
-
-
-# In[ ]:
-
-
-#No podemos incrustar el objeto "data" puesto que es una lista de documentos, lo que espera es una string
-#embedded_docs = embeddings.embed_documents(data)
-
-
-# In[ ]:
-
-
-#Creamos una comprensión de listas concatenando el campo "page_content" de todos los documentos existentes en la lista "data"
-[elemento.page_content for elemento in data]
-
-
-# In[ ]:
-
+# No podemos incrustar el objeto "data" puesto que es una lista de documentos, lo que espera es una string
+# embedded_docs = embeddings.embed_documents(data) Creamos una comprensión de listas concatenando el campo
+# "page_content" de todos los documentos existentes en la lista "data"
+# [elemento.page_content for elemento in data]
 
 embedded_docs = embeddings.embed_documents([elemento.page_content for elemento in data])
+# Verificamos cuántos vectores ha creado (1 por cada registro del fichero CSV con datos)
+print('\n Length of embedded docs:')
+print(len(embedded_docs))
 
-
-# In[ ]:
-
-
-#Verificamos cuántos vectores a creado (1 por cada registro del fichero CSV con datos)
-len(embedded_docs)
-
-
-# In[ ]:
-
-
-#Vemos un ejemplo del vector creado para el primer registro
-embedded_docs[1]
-
-
-# In[ ]:
-
-
-
-
+# Vemos un ejemplo del vector creado para el primer registro
+print('\n Example of first element of the embedded docs:')
+print(embedded_docs[1])
