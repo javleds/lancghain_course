b49df80cccab114896f8f7811933d6cfdc4cf530
Javier Ledezma <juls0593@gmail.com>
2024-09-10 21:03:47 -0600

    Fix unit 4, 2nd script AS IS

--- unit_4_chanins/02_suquential_model.py
+++ unit_4_chanins/02_suquential_model.py
@@ -1,70 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# #  Modelo Secuencial Completo
-
-# ## Importar librerías e instancia de modelo de chat
-
-# In[ ]:
-
-
-from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate,ChatPromptTemplate, HumanMessagePromptTemplate
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.chains import SequentialChain, LLMChain #importamos el SequentialChain que es el modelo completo
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-llm = ChatOpenAI(openai_api_key=api_key)
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import SequentialChain, LLMChain
 
-
-# In[ ]:
-
+load_dotenv()
+llm = ChatOpenAI()
 
 template1 = "Dame un resumen del rendimiento de este trabajador:\n{revision_rendimiento}"
 prompt1 = ChatPromptTemplate.from_template(template1)
-chain_1 = LLMChain(llm=llm,
-                     prompt=prompt1,
-                     output_key="resumen_revision")
-
-#Opciones objetos runnables: chain_1= prompt1 | llm
-
-
-# In[ ]:
-
+chain_1 = LLMChain(llm=llm, prompt=prompt1, output_key="resumen_revision")
 
 template2 = "Identifica las debilidades de este trabajador dentro de de este resumen de la revisión:\n{resumen_revision}"
 prompt2 = ChatPromptTemplate.from_template(template2)
-chain_2 = LLMChain(llm=llm,
-                     prompt=prompt2,
-                     output_key="debilidades")
-
-#Opciones objetos runnables: chain_2= prompt2 | llm
-
-
-# In[ ]:
-
+chain_2 = LLMChain(llm=llm, prompt=prompt2, output_key="debilidades")
 
 template3 = "Crea un plan de mejora para ayudar en estas debilidades:\n{debilidades}"
 prompt3 = ChatPromptTemplate.from_template(template3)
-chain_3 = LLMChain(llm=llm,
-                     prompt=prompt3,
-                     output_key="plan_mejora")
+chain_3 = LLMChain(llm=llm, prompt=prompt3, output_key="plan_mejora")
 
-#Opciones objetos runnables: chain_3= prompt3 | llm
-
-
-# In[ ]:
-
-
-seq_chain = SequentialChain(chains=[chain_1,chain_2,chain_3],
+# Change verbose=False to more details
+seq_chain = SequentialChain(chains=[chain_1, chain_2, chain_3],
                             input_variables=['revision_rendimiento'],
-                            output_variables=['resumen_revision','debilidades','plan_mejora'],
-                            verbose=True)
-
-
-# In[ ]:
-
+                            output_variables=['resumen_revision', 'debilidades', 'plan_mejora'],
+                            verbose=False)
 
 revision_rendimiento_empleado = '''
 Revisión de Rendimiento del Empleado
@@ -80,29 +41,9 @@ Debilidades:
 A pesar de sus muchas fortalezas, Juan ha mostrado áreas que necesitan mejoras. En particular, se ha observado que a veces tiene dificultades para manejar múltiples tareas simultáneamente, lo que resulta en retrasos en la entrega de proyectos. También ha habido ocasiones en las que la calidad del trabajo ha disminuido bajo presión. Además, se ha identificado una necesidad de mejorar sus habilidades de comunicación, especialmente en lo que respecta a la presentación de datos complejos de manera clara y concisa a los miembros no técnicos del equipo. Finalmente, se ha notado una falta de proactividad en la búsqueda de soluciones a problemas imprevistos, confiando a menudo en la orientación de sus superiores en lugar de tomar la iniciativa.
 '''
 
-
-# In[ ]:
-
-
 results = seq_chain.invoke(revision_rendimiento_empleado)
 
-
-# In[ ]:
-
-
-results #Tenemos un diccionario con todos los resultados intermedios y final
-
-
-# In[ ]:
-
-
-#Resultado final
+print('\nPlan de mejora:')
 print(results['plan_mejora'])
-
-
-# In[ ]:
-
-
-#Se puede accceder a los resultados intermedios:
+print('\nDebilidades:')
 print(results["debilidades"])
-
