102f3b79194484f7eee59c735d75bd10b07a51dc
Javier Ledezma <juls0593@gmail.com>
2024-09-10 22:36:49 -0600

    Fix unit 5, 3rd script

--- unit_5_memory/03_window_buffer_memory.py
+++ unit_5_memory/03_window_buffer_memory.py
@@ -1,53 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# ## Importar librerías iniciales e instancia de modelo de chat
-
-# In[ ]:
-
-
+from dotenv import load_dotenv
 from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
+from langchain.memory import ConversationBufferWindowMemory
 from langchain_openai import ChatOpenAI
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-llm = ChatOpenAI(openai_api_key=api_key)
 
+load_dotenv()
+llm = ChatOpenAI()
 
-# #  Crear objeto ConversationBufferWindowMemory
+# k indica el número de iteraciones (pareja de mensajes human-AI) que guardar en la memoria
+memory = ConversationBufferWindowMemory(k=1)
 
-# In[ ]:
-
-
-memory = ConversationBufferWindowMemory(k=1) #k indica el número de iteraciones (pareja de mensajes human-AI) que guardar
-
-
-# ## Conectar una conversación a la memoria
-
-# In[ ]:
-
-
-#Creamos una instancia de la cadena conversacional con el LLM y el objeto de memoria
-conversation = ConversationChain(llm=llm,memory = memory,verbose=True)
-#Ejemplo con RunnableWithMessageHistory: https://api.python.langchain.com/en/latest/runnables/langchain_core.runnables.history.RunnableWithMessageHistory.html
-
-
-# In[ ]:
-
-
+# Ejemplo con RunnableWithMessageHistory:
+# https://api.python.langchain.com/en/latest/runnables/langchain_core.runnables.history.RunnableWithMessageHistory.html
+conversation = ConversationChain(llm=llm, memory=memory, verbose=False)
 conversation.predict(input="Hola, ¿cómo estás?")
-
-
-# In[ ]:
-
-
 conversation.predict(input="Necesito un consejo para tener un gran día")
 
-
-# In[ ]:
-
-
-print(memory.buffer) #k limita el número de interacciones
-
+print('\nMemory buffer:')
+print(memory.buffer)
