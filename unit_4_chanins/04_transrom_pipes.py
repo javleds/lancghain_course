35e9a9db2d73a5ae37f2443fe8e5235eb0437f30
Javier Ledezma <juls0593@gmail.com>
2024-09-10 22:11:01 -0600

    Fix unit 4, 4rd script

--- unit_4_chanins/04_transrom_pipes.py
+++ unit_4_chanins/04_transrom_pipes.py
@@ -1,120 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# ## Importar librerías iniciales e instancia de modelo de chat
-
-# In[ ]:
-
-
-from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate,ChatPromptTemplate, HumanMessagePromptTemplate
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import SimpleSequentialChain, LLMChain,TransformChain
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-llm = ChatOpenAI(openai_api_key=api_key)
+from langchain.chains import SimpleSequentialChain, LLMChain, TransformChain
+from langchain_community.document_loaders import WikipediaLoader
 
+load_dotenv()
+llm = ChatOpenAI()
 
-# ## Importamos documentos
+consulta_wikipedia = input('Introduce el tema sobre el que quieres buscar información en Wikipedia: ')
+idioma_final = input('Introduce el idioma al que quieres traducir el texto: ')
 
-# In[ ]:
-
-
-from langchain.document_loaders import WikipediaLoader
-
-
-# In[ ]:
-
-
-consulta_wikipedia = input()
-
-
-# In[ ]:
-
-
-idioma_final = input()
-
-
-# In[ ]:
-
-
-loader = WikipediaLoader(query=consulta_wikipedia,lang="es",load_max_docs=10)
-
-
-# In[ ]:
-
-
+loader = WikipediaLoader(query=consulta_wikipedia, lang="es", load_max_docs=10)
 data = loader.load()
 
-
-# In[ ]:
-
-
-data[0].page_content
-
-
-# In[ ]:
-
+print('\nWikepedia data:')
+print(data[0].page_content)
 
 texto_entrada = data[0].page_content
 
 
-# # TransformChain
-
-# ### Definir la función de transformación personalizada
-
-# In[ ]:
-
-
-def transformer_function(inputs: dict) -> dict: #Toma de entrada un diccionario y lo devuelve con la transformación oportuna
+def transformer_function(inputs: dict) -> dict:
     texto = inputs['texto']
     primer_parrafo = texto.split('\n')[0]
-    return {'salida':primer_parrafo}
+    return {'salida': primer_parrafo}
 
 
-# In[ ]:
+transform_chain = TransformChain(input_variables=['texto'], output_variables=['salida'], transform=transformer_function)
 
-
-transform_chain = TransformChain(input_variables=['texto'],
-                                 output_variables=['salida'],
-                                 transform=transformer_function)
-
-
-# ## Definir la cadena secuencial
-
-# In[ ]:
-
-
-#Creamos bloque LLMChain para resumir
 template1 = "Crea un resumen en una línea del siguiente texto:\n{texto}"
 prompt = ChatPromptTemplate.from_template(template1)
-summary_chain = LLMChain(llm=llm,
-                     prompt=prompt,
-                     output_key="texto_resumen")
+summary_chain = LLMChain(llm=llm, prompt=prompt, output_key="texto_resumen")
 
-
-# In[ ]:
-
-
-#Creamos bloque LLMChain para traducir
-template2 = "Traduce a"+ idioma_final + "el siguiente texto:\n{texto}"
+template2 = "Traduce a" + idioma_final + "el siguiente texto:\n{texto}"
 prompt = ChatPromptTemplate.from_template(template2)
-#prompt.format_prompt(idioma=idioma_final)
-translate_chain = LLMChain(llm=llm,
-                     prompt=prompt,
-                     output_key="texto_traducido")
+translate_chain = LLMChain(llm=llm, prompt=prompt, output_key="texto_traducido")
 
-
-# In[ ]:
-
-
-sequential_chain = SimpleSequentialChain(chains=[transform_chain,summary_chain,translate_chain],
-                                        verbose=True)
-
-
-# In[ ]:
-
+sequential_chain = SimpleSequentialChain(
+    chains=[transform_chain, summary_chain, translate_chain],
+    verbose=True)
 
 result = sequential_chain(texto_entrada)
 
+print('\nResultado:')
+print(result)
