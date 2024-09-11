d4420ff1bc78b860124f7ad48e51b9a2c50941f9
Javier Ledezma <juls0593@gmail.com>
2024-09-10 19:59:50 -0600

    Formatting the fourth script

--- unit_2_io_model/04_promt_serialization.py
+++ unit_2_io_model/04_promt_serialization.py
@@ -1,46 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# ## Importar librerías e instancia de modelo de chat
-
-# In[ ]:
-
-
-from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate,ChatPromptTemplate, HumanMessagePromptTemplate
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-chat = ChatOpenAI(openai_api_key=api_key)
+from langchain.prompts import (
+    load_prompt,
+    PromptTemplate
+)
 
+load_dotenv()
+chat = ChatOpenAI()
 
-# ### Guardar plantilla prompt
-
-# In[ ]:
-
-
+# Guardar plantilla prompt
 plantilla = "Pregunta: {pregunta_usuario}\n\nRespuesta: Vamos a verlo paso a paso."
 prompt = PromptTemplate(template=plantilla)
 prompt.save("prompt.json")
 
-
 # ### Cargar plantilla prompt
-
-# In[ ]:
-
-
-from langchain.prompts import load_prompt
-
-
-# In[ ]:
-
-
 prompt_cargado = load_prompt('prompt.json')
 
-
-# In[ ]:
-
-
-prompt_cargado
-
+print(prompt_cargado)
