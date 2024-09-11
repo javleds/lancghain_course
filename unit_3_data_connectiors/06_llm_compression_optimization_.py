b19e432df27a49e1a7694f1455259150dec7a936
Javier Ledezma <juls0593@gmail.com>
2024-09-10 20:49:57 -0600

    Fix unit 2, 6th script implement a file check before

--- unit_3_data_connectiors/06_llm_compression_optimization_.py
+++ unit_3_data_connectiors/06_llm_compression_optimization_.py
@@ -1,132 +1,58 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# ## 0. Importar librerías
-
-# In[ ]:
-
-
-from langchain.document_loaders import WikipediaLoader
-from langchain_openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import TextLoader
-from langchain_community.vectorstores import SKLearnVectorStore
-
-
-# ## 1. Carga de documentos
-
-# In[ ]:
-
-
-loader = WikipediaLoader(query='Lenguaje Python',lang="es")
-documents = loader.load()
-
-
-# In[ ]:
-
-
-documents
-
-
-# In[ ]:
-
-
-len(documents)
-
-
-# ## 2. Split de Documentos
-
-# In[ ]:
-
-
-# División en fragmentos
-text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=500)
-docs = text_splitter.split_documents(documents)
-
-
-# In[ ]:
-
-
-len(docs)
-
-
-# ## 3. Conectar a OpenAI para los embeddings
-
-# In[ ]:
-
-
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-funcion_embedding = OpenAIEmbeddings(openai_api_key=api_key)
-
-
-# ## 4. Incrustar documentos en BD Vectores
-
-# In[ ]:
-
-
-persist_path="./ejemplo_wiki_bd"  #ruta donde se guardará la BBDD vectorizada
-
-#Creamos la BBDD de vectores a partir de los documentos y la función embeddings
-vector_store = SKLearnVectorStore.from_documents(
-    documents=docs,
-    embedding=funcion_embedding,
-    persist_path=persist_path,
-    serializer="parquet", #el serializador o formato de la BD lo definimos como parquet
-)
-
-
-# In[ ]:
-
-
-# Fuerza a guardar los nuevos embeddings en el disco
-vector_store.persist()
-
-
-# ## 5a. Consulta normal similitud coseno
-
-# In[ ]:
-
-
-#Creamos un nuevo documento que será nuestra "consulta" para buscar el de mayor similitud en nuestra Base de Datos de Vectores y devolverlo
-consulta = "¿Por qué el lenguaje Python se llama así?"
-docs = vector_store.similarity_search(consulta)
-print(docs[0].page_content)
-
-
-# ## 5b. Consulta con compresión contextual usando LLMs
-
-# In[ ]:
-
-
-from langchain_openai import ChatOpenAI
+from pathlib import Path
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain_community.document_loaders import WikipediaLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import SKLearnVectorStore
 
+load_dotenv()
 
-# In[ ]:
+embedding_function = OpenAIEmbeddings()
+persist_path = "./ejemplo_wiki_bd"
 
+if Path('ejemplo_wiki_bd').exists():
+    print('\nLoading local vector store')
+    vector_store = SKLearnVectorStore(
+        embedding=embedding_function, persist_path=persist_path, serializer="parquet"
+    )
+else:
+    print('\nCreating new local vector store')
+    loader = WikipediaLoader(query='Lenguaje Python', lang="es")
+    documents = loader.load()
 
-llm = ChatOpenAI(temperature=0,openai_api_key=api_key) #el parámetro temperatura define la aleatoriedad de las respuestas, temperatura = 0 significa el mínimo de aleatoriedad
+    # División en fragmentos
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=500)
+    docs = text_splitter.split_documents(documents)
+
+    # Creamos la DB de vectores a partir de los documentos y la función embeddings
+    vector_store = SKLearnVectorStore.from_documents(
+        documents=docs,
+        embedding=embedding_function,
+        persist_path=persist_path,
+        serializer="parquet",  # el serializador o formato de la BD lo definimos como parquet
+    )
+
+    vector_store.persist()
+
+# 5a. Consulta normal similitud coseno Creamos un nuevo documento que será nuestra "consulta" para buscar el de
+# mayor similitud en nuestra Base de Datos de Vectores y devolverlo
+consulta = "¿Por qué el lenguaje Python se llama así?"
+docs = vector_store.similarity_search(consulta)
+print('\n5a. Consulta local:')
+print(docs[0].page_content)
+
+# 5b. Consulta con compresión contextual usando LLMs
+llm = ChatOpenAI(temperature=0)
 compressor = LLMChainExtractor.from_llm(llm)
 
-
-# In[ ]:
-
-
-compression_retriever = ContextualCompressionRetriever(base_compressor=compressor, base_retriever=vector_store.as_retriever())
-
-
-# In[ ]:
-
+compression_retriever = ContextualCompressionRetriever(base_compressor=compressor,
+                                                       base_retriever=vector_store.as_retriever())
 
 compressed_docs = compression_retriever.invoke("¿Por qué el lenguaje Python se llama así?")
-
-
-# In[ ]:
-
-
-compressed_docs[0].page_content
-
+print('\n5b. Complemento con OpenAI:')
+print(compressed_docs[0].page_content)
