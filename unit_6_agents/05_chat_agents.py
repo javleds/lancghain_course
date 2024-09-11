df1be061237f3a489f49e11bd4e6c1852ed5a115
Javier Ledezma <juls0593@gmail.com>
2024-09-11 10:02:03 -0600

    Fix unit 6, 5th script

--- unit_6_agents/05_chat_agents.py
+++ unit_6_agents/05_chat_agents.py
@@ -1,61 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# ## Importar librerías iniciales e instancia de modelo de chat
-
-# In[1]:
-
-
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate,ChatPromptTemplate, HumanMessagePromptTemplate
-from langchain.agents import load_tools,initialize_agent,AgentType,create_react_agent,AgentExecutor
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-llm = ChatOpenAI(openai_api_key=api_key,temperature=0) #Recomendable temperatura a 0 para que el LLM no sea muy creativo, vamos a tener muchas herramientas a nuestra disposición y queremos que sea más determinista
-
-
-# # CASO DE USO 1
-
-# In[2]:
-
-
+from langchain.agents import load_tools, initialize_agent, AgentType
 from langchain.memory import ConversationBufferMemory
 
+load_dotenv()
+llm = ChatOpenAI(temperature=0)
 
-# In[3]:
+# ponemos una denominada clave a la memoria "chat_history"
+memory = ConversationBufferMemory(memory_key="chat_history")
+tools = load_tools(["wikipedia", "llm-math", ], llm=llm)
 
+agent = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, memory=memory, verbose=True)
 
-memory = ConversationBufferMemory(memory_key="chat_history") #ponemos una denominada clave a la memoria "chat_history"
+print('\n5 Productos mantenimiento vehiculo:')
+print(agent.invoke({'input': 'Dime 5 productos esenciales para el mantenimiento del vehículo.'}))
 
+print('\nEl más importante:')
+print(agent.invoke({'input': '¿Cuál de los anteriores es el más importante?'}))
 
-# In[4]:
-
-
-tools = load_tools(["wikipedia","llm-math",],llm=llm)
-
-
-# In[6]:
-
-
-agent = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,memory=memory,verbose=True)
-
-
-# In[7]:
-
-
-agent.invoke("Dime 5 productos esenciales para el mantenimiento del vehículo.")
-
-
-# In[8]:
-
-
-agent.invoke("¿Cuál de los anteriores es el más importante?")
-
-
-# In[9]:
-
-
-agent.invoke("Necesito la respuesta anterior en castellano")
-
+print('\nEn inglés:')
+print(agent.invoke({'input': 'Necesito la respuesta anterior en inglés'}))
