e289cf51f8ad2f58e5abd9af15e40e06a815b362
Javier Ledezma <juls0593@gmail.com>
2024-09-11 09:40:33 -0600

    Fix unit 6, 3rd script

--- unit_6_agents/03_code_generator_agent.py
+++ unit_6_agents/03_code_generator_agent.py
@@ -1,97 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# ## Importar librerías iniciales e instancia de modelo de chat
-
-# In[ ]:
-
-
+# pip install pandas seaborn
+import pandas as pd
+import seaborn as sns
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate,ChatPromptTemplate, HumanMessagePromptTemplate
-from langchain.agents import load_tools,initialize_agent,AgentType,create_react_agent,AgentExecutor
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-llm = ChatOpenAI(openai_api_key=api_key,temperature=0) #Recomendable temperatura a 0 para que el LLM no sea muy creativo, vamos a tener muchas herramientas a nuestra disposición y queremos que sea más determinista
-
-
-# In[ ]:
-
-
+from langchain.agents import AgentType
 from langchain_experimental.agents.agent_toolkits import create_python_agent
 from langchain_experimental.tools.python.tool import PythonREPLTool
 
+load_dotenv()
+llm = ChatOpenAI(temperature=0)
 
-# ## Creamos el agente para crear y ejecutar código Python 
-
-# In[ ]:
-
-
+# ## Creamos el agente para crear y ejecutar código Python
 agent = create_python_agent(tool=PythonREPLTool(),
-                           llm=llm,
-                           agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
+                            llm=llm,
+                            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
 
-
-# In[ ]:
-
-
-lista_ejemplo = [3,1,5,3,5,6,7,3,5,10]
-
-
-# In[ ]:
-
-
-agent.invoke(f'''ordena la lista {lista_ejemplo}''')
-
-
-# ## Ejemplo con un dataframe
-
-# In[ ]:
-
-
-import pandas as pd
-
-
-# In[ ]:
-
+lista_ejemplo = [3, 1, 5, 3, 5, 6, 7, 3, 5, 10]
+agent.invoke({'input': f'''ordena la lista {lista_ejemplo}'''})
 
 df = pd.read_excel('datos_ventas_small.xlsx')
 
-
-# In[ ]:
-
-
-df.head()
-
-
-# In[ ]:
-
-
-agent.invoke(f'''¿Qué sentencias de código tendría que ejecutar para obtener la suma de venta total agregada por Línea de Producto? Este sería el dataframe {df}, no tienes que ejecutar la sentencia, solo pasarme el código a ejecutar''')
-
-
-# In[ ]:
-
+print(df.head())
+print(agent.invoke({
+    'input': f'''¿Qué sentencias de código tendría que ejecutar para obtener la suma de venta total agregada por Línea de Producto? Este sería el dataframe {df}, no tienes que ejecutar la sentencia, solo pasarme el código a ejecutar''',
+}))
 
 df.groupby('Línea Producto')['Venta total'].sum()
 
+print(agent.invoke({
+    'input': f'''¿Cuál es la suma agregada de la venta total para la línea de proudcto "Motorcycles"? Este sería el dataframe {df}'''
+}))
 
-# In[ ]:
+print(agent.invoke({
+    'input': f'''¿Qué sentencias de código tendría que ejecutar para tener una visualización con la librería Seaborn que agregue a nivel de Línea de Producto el total de venta? Este sería el dataframe {df}, recuerda que no tienes que ejecutar la sentencia, solo pasarme el código a ejecutar'''
+}))
 
-
-agent.invoke(f'''¿Cuál es la suma agregada de la venta total para la línea de proudcto "Motorcycles"? Este sería el dataframe {df}''')
-
-
-# In[ ]:
-
-
-agent.invoke(f'''¿Qué sentencias de código tendría que ejecutar para tener una visualización con la librería Seaborn que agregue a nivel de Línea de Producto el total de venta? Este sería el dataframe {df}, recuerda que no tienes que ejecutar la sentencia, solo pasarme el código a ejecutar''')
-
-
-# In[ ]:
-
-
-import seaborn as sns
 sns.barplot(x='Línea Producto', y='Venta total', data=df, estimator=sum)
-
