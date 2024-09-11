1cad201d2c75efe7fd6d1e4286c4ca49e0aa4bdb
Javier Ledezma <juls0593@gmail.com>
2024-09-10 22:39:02 -0600

    Fix unit 5, 4th script

--- unit_5_memory/04_summary_buffer_memory.py
+++ unit_5_memory/04_summary_buffer_memory.py
@@ -1,61 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# ## Importar librerías iniciales e instancia de modelo de chat
-
-# In[ ]:
-
-
+from dotenv import load_dotenv
 from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
+from langchain.memory import ConversationSummaryBufferMemory
 from langchain_openai import ChatOpenAI
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-llm = ChatOpenAI(openai_api_key=api_key)
 
-
-# #  Crear objeto ConversationSummaryMemory
-
-# In[ ]:
-
-
-memory = ConversationSummaryBufferMemory(llm=llm)
-
-
-# In[ ]:
-
+load_dotenv()
+llm = ChatOpenAI()
 
 # Creamos un prompt cuya respuesta hará que se sobrepase el límite de tokens y por tanto sea recomendable resumir la memoria
 plan_viaje = '''Este fin de semana me voy de vacaciones a la playa, estaba pensando algo que fuera bastante relajado, pero necesito 
 un plan detallado por días con qué hacer en familia, extiéndete todo lo que puedas'''
 
-
-# ## Creamos una nueva conversación con un buffer de memoria resumida
-
-# In[ ]:
-
-
+# Ejemplo con RunnableWithMessageHistory:
+# https://api.python.langchain.com/en/latest/runnables/langchain_core.runnables.history.RunnableWithMessageHistory.html
 memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=100)
-conversation = ConversationChain(llm=llm,memory = memory,verbose=True)
-#Ejemplo con RunnableWithMessageHistory: https://api.python.langchain.com/en/latest/runnables/langchain_core.runnables.history.RunnableWithMessageHistory.html
-
-
-# In[ ]:
-
-
+conversation = ConversationChain(llm=llm, memory=memory, verbose=False)
 conversation.predict(input=plan_viaje)
+memory.load_memory_variables({})  # Se ha realizado un resumen de la memoria en base al límite de tokens
 
-
-# In[ ]:
-
-
-memory.load_memory_variables({}) #Se ha realizado un resumen de la memoria en base al límite de tokens
-
-
-# In[ ]:
-
-
+print('\nMemory buffer:')
 print(memory.buffer)
-
