6f728488a6068ba4239bfe02666659ff48d12c50
Javier Ledezma <juls0593@gmail.com>
2024-09-10 20:19:05 -0600

    Fix unit 2, 2nd script

--- unit_3_data_connectiors/02_document_loader_integrations.py
+++ unit_3_data_connectiors/02_document_loader_integrations.py
@@ -1,56 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <em style="text-align:center">Copyright Iván Pinar Domínguez</em>
-
-# ## Importar librerías e instancia de modelo de chat
-
-# In[ ]:
-
-
-from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate,ChatPromptTemplate, HumanMessagePromptTemplate
+# pip install wikipedia en una terminal
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-f = open('../OpenAI_key.txt')
-api_key = f.read()
-chat = ChatOpenAI(openai_api_key=api_key)
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_community.document_loaders import WikipediaLoader
+
+load_dotenv()
+chat = ChatOpenAI()
 
 
-# #  Integración Wikipedia
-
-# In[ ]:
-
-
-from langchain.document_loaders import WikipediaLoader # pip install wikipedia en una terminal
-
-
-# In[ ]:
-
-
-def responder_wikipedia(persona,pregunta_arg):
+def responder_wikipedia(persona: str, pregunta_arg: str) -> str:
     # Obtener artículo de wikipedia
-    docs = WikipediaLoader(query=persona,lang="es",load_max_docs=10) #parámetros posibles en: https://python.langchain.com/v0.2/docs/integrations/document_loaders/wikipedia/
-    contexto_extra = docs.load()[0].page_content #para que sea más rápido solo pásamos el primer documento [0] como contexto extra
-    
+    # parámetros posibles en: https://python.langchain.com/v0.2/docs/integrations/document_loaders/wikipedia/
+    docs = WikipediaLoader(query=persona, lang="es",
+                           load_max_docs=10)
+    # para que sea más rápido solo pasamos el primer documento [0] como contexto extra
+    contexto_extra = docs.load()[0].page_content
+
     # Pregunta de usuario
-    human_prompt = HumanMessagePromptTemplate.from_template('Responde a esta pregunta\n{pregunta}, aquí tienes contenido extra:\n{contenido}')
-    
+    human_prompt = HumanMessagePromptTemplate.from_template(
+        'Responde a esta pregunta\n{pregunta}, aquí tienes contenido extra:\n{contenido}')
+
     # Construir prompt
     chat_prompt = ChatPromptTemplate.from_messages([human_prompt])
-    
+
     # Resultado
-    result = chat.invoke(chat_prompt.format_prompt(pregunta=pregunta_arg,contenido=contexto_extra).to_messages())
-    
-    print(result.content)
+    result = chat.invoke(chat_prompt.format_prompt(pregunta=pregunta_arg, contenido=contexto_extra).to_messages())
+
+    return str(result.content)
 
 
-# In[ ]:
+respuesta = responder_wikipedia("Fernando Alonso", "¿Cuándo nació?")
 
-
-responder_wikipedia("Fernando Alonso","¿Cuándo nació?")
-
-
-# In[ ]:
-
-
-
-
+print(respuesta)
